af7b614e2c5f73861573ae758058b5da4dfdc184
gigabyte <1099511627776@mail.ru>
2013-03-01 20:05:53 +0200

    Preparing fore LS Store

---
+++ follower/classes/hooks/HookFollower.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class PluginFollower_HookFollower extends Hook {
+
+    /*
+     * Регистрация событий на хуки
+	*/
+    public function RegisterHook() {
+        $this->AddHook('template_footer_end', 'displayCopyright',__CLASS__);
+    }
+	
+	public function displayCopyright(){
+		$s = '<a href="http://goloskarpat.info/">goloskarpat.info</a>';
+		dump($s);
+        return $s;
+	}
+}
+?>
--- follower/config/config.php
+++ follower/config/config.php
@@ -1,4 +1,22 @@
 <?
 $config = array();
-$config['domains'] = array();
+
+/* 
+	Список доменов которые мы будем фоловить
+	List of domain being followed
+*/
+
+$config['domains'] = array(
+	"http\:\/\/goloskarpat\.info",	
+);
+
+/*
+	Список ИД пользователей в топиках которых 
+	все ссылки автоматически будут фоловится
+
+	List of user IDs all links in topics
+	will be followed
+*/
+$config['autofollow'] = array(1);
+
 return $config;
