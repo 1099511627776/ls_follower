b01561a51a77982383641a6f88f3da477af40dd0
gigabyte <1099511627776@mail.ru>
2013-03-06 09:02:59 +0200

    Some bugfixes from site

--- follower/classes/modules/text/Text.class.php
+++ follower/classes/modules/text/Text.class.php
@@ -1,31 +1,13 @@
-<?
+<?php
+
 class PluginFollower_ModuleText extends PluginFollower_Inherit_ModuleText
 {
 	public function LoadJevixConfig($sType='default',$bClear=true) {
-		if ($bClear) {
-			$this->oJevix->tagsRules=array();
-		}
-		$aConfig=Config::Get('jevix.'.$sType);
-		if (is_array($aConfig)) {
-			foreach ($aConfig as $sMethod => $aExec) {
-				foreach ($aExec as $aParams) {
-					if (in_array(strtolower($sMethod),array_map("strtolower",array('cfgSetTagCallbackFull','cfgSetTagCallback')))) {
-						if (isset($aParams[1][0]) and $aParams[1][0]=='_this_') {
-							$aParams[1][0]=$this;
-						}
-					}
-					call_user_func_array(array($this->oJevix,$sMethod), $aParams);
-				}
-			}
-			unset($this->oJevix->entities1['&']);
-			if (Config::Get('view.noindex') and isset($this->oJevix->tagsRules['a'])) {				
-				$this->oJevix->cfgSetTagParamDefault('a','rel','nofollow',true);
-				if($oUser = $this->User_GetUserCurrent()){
-					if($autofollow = Config::Get('plugin.follower.autofollow')){
-						if(in_array($oUser->getId(),$autofollow)){
-							$this->oJevix->cfgSetTagParamDefault('a','rel','follow',true);
-						}
-					}
+		parent::LoadJevixConfig($sType,$bClear);
+		if($oUser = $this->User_GetUserCurrent()){
+			if($autofollow = Config::Get('plugin.follower.autofollow')){
+				if(in_array($oUser->getId(),$autofollow)){
+					$this->oJevix->cfgSetTagParamDefault('a','rel','',true);
 				}
 			}
 		}
@@ -38,12 +20,13 @@ class PluginFollower_ModuleText extends PluginFollower_Inherit_ModuleText
 			"<a $1 href=\"$2\" $3>",$sText);
 		$sResult = preg_replace(
 			"/<a\s+rel=[\"'](.*?)[\"'](.*?)href=[\"'](.*".$sDomain.".*?)[\"'](.*?)>/mi",
-			"<a href=\"$3\" rel=\"follow\" $2 $4>",$sResult);
+			"<a href=\"$3\" $2 $4>",$sResult);
 		$sResult = preg_replace(
 			"/<a\s+href=[\"'](.*?".$sDomain.".*?)[\"'](.*?)rel=[\"](.*?)[\"](.*?)>/mi",
-			"<a href=\"$1\" $2 rel=\"follow\" $4>",$sResult);
+			"<a href=\"$1\" $2 $4>",$sResult);
 		return $sResult;
 	}
+
 	protected function FollowDomains($sText){
 	    $sResult = $sText;
 		if($domains = Config::Get('plugin.follower.domains')){			
